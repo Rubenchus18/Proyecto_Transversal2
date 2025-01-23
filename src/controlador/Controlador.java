0bfbc1a6097d2f81b3d5c0367115638d1ecf796b
Fermin <fermin4002@gmail.com>
2025-01-23 13:34:27 +0100

    creacion

---
+++ src/controlador/Controlador.java
@@ -0,0 +1,19 @@
+package controlador;
+
+import vista.*;
+import modelo.*;
+
+
+
+public class Controlador {
+
+	private Vista vista;
+	
+	public Controlador(Vista vista) {
+		
+		this.vista=vista;
+		
+	}
+	
+	
+}
