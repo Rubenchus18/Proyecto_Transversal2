c97944751b0b643112c16f788c974c9cf93dcebe
Rubenchus18 <rubengarci327@gmail.com>
2025-02-04 13:23:55 +0100

    Nuevas camisetas

--- src/vista/Vista.java
+++ src/vista/Vista.java
@@ -16,6 +16,8 @@ import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Vista extends JFrame {
 
@@ -46,6 +48,7 @@ public class Vista extends JFrame {
 	public JLabel lblFondoPlantilla;
 	public JLabel lblFondoDraft;
 	public JLabel lblSalir;
+	public Object lblSaliraddMouseLis;
 	/**
 	 * Launch the application.
 	 */
@@ -90,77 +93,122 @@ public class Vista extends JFrame {
 		   PanelPlantilla.setLayout(null);
 		    
 		     panelElecion = new JPanel();
-		    panelElecion.setBounds(353, 148, 651, 324);
+		    panelElecion.setBounds(249, 148, 844, 324);
 		    panelElecion.setVisible(false);
 		    PanelPlantilla.add(panelElecion);
 		    panelElecion.setLayout(null);
 		     
+		     JButton btnEleccionTres_1 = new JButton("");
+		     btnEleccionTres_1.setBounds(359, 29, 118, 285);
+		     panelElecion.add(btnEleccionTres_1);
+		     
 		     lblSalir = new JLabel("");
-		     lblSalir.setBounds(606, 0, 45, 31);
+		     lblSalir.setBounds(799, 0, 45, 31);
 		     panelElecion.add(lblSalir);
 		    
 		     btnEleccionUno = new JButton("");
-		    btnEleccionUno.setBounds(43, 29, 116, 285);
+		     btnEleccionUno.addActionListener(new ActionListener() {
+		     	public void actionPerformed(ActionEvent e) {
+		     	}
+		     });
+		    btnEleccionUno.setBounds(41, 29, 116, 285);
 		    panelElecion.add(btnEleccionUno);
 		    
 		     btnEleccionDos = new JButton("");
-		    btnEleccionDos.setBounds(188, 29, 117, 285);
+		     btnEleccionDos.addActionListener(new ActionListener() {
+		     	public void actionPerformed(ActionEvent e) {
+		     	}
+		     });
+		    btnEleccionDos.setBounds(196, 29, 117, 285);
 		    panelElecion.add(btnEleccionDos);
 		    
 		     btnEleccionTres = new JButton("");
-		    btnEleccionTres.setBounds(345, 29, 118, 285);
+		    btnEleccionTres.setBounds(525, 29, 118, 285);
 		    panelElecion.add(btnEleccionTres);
 		    
 		     btnEleccionCuatro = new JButton("");
-		    btnEleccionCuatro.setBounds(495, 29, 116, 285);
+		    btnEleccionCuatro.setBounds(684, 29, 116, 285);
 		    panelElecion.add(btnEleccionCuatro);
 		    
 		    lblFondoDraft = new JLabel("");
-		    lblFondoDraft.setBounds(0, 0, 651, 324);
+		    lblFondoDraft.setBounds(0, 0, 844, 324);
 		    panelElecion.add(lblFondoDraft);
 		    
 		    btnPortero = new JButton("");
 		    btnPortero.setBounds(626, 622, 85, 91);
+		    btnPortero.setContentAreaFilled(false); 
+		    btnPortero.setBorderPainted(false); 
+		    btnPortero.setFocusPainted(false);
 		    PanelPlantilla.add(btnPortero);
 		    
 		    btnDefensaIzquierda = new JButton("");
 		    btnDefensaIzquierda.setBounds(870, 482, 85, 91);
+		    btnDefensaIzquierda.setContentAreaFilled(false); 
+		    btnDefensaIzquierda.setBorderPainted(false); 
+		    btnDefensaIzquierda.setFocusPainted(false);
 		    PanelPlantilla.add(btnDefensaIzquierda);
 		    
 		    btnCentroCampistaIzquierdoCentro = new JButton("");
 		    btnCentroCampistaIzquierdoCentro.setBounds(747, 343, 85, 91);
+		    btnCentroCampistaIzquierdoCentro.setContentAreaFilled(false); 
+		    btnCentroCampistaIzquierdoCentro.setBorderPainted(false); 
+		    btnCentroCampistaIzquierdoCentro.setFocusPainted(false);
 		    PanelPlantilla.add(btnCentroCampistaIzquierdoCentro);
 		    
 		    btnDefensaIzquierdaCentro = new JButton("");
 		    btnDefensaIzquierdaCentro.setBounds(747, 482, 85, 91);
+		    btnDefensaIzquierdaCentro.setContentAreaFilled(false); 
+		    btnDefensaIzquierdaCentro.setBorderPainted(false); 
+		    btnDefensaIzquierdaCentro.setFocusPainted(false);
 		    PanelPlantilla.add(btnDefensaIzquierdaCentro);
 		    
 		    btnDefensaDerechaCentro = new JButton("");
 		    btnDefensaDerechaCentro.setBounds(525, 482, 85, 91);
+		    btnDefensaDerechaCentro.setContentAreaFilled(false); 
+		    btnDefensaDerechaCentro.setBorderPainted(false); 
+		    btnDefensaDerechaCentro.setFocusPainted(false);
 		    PanelPlantilla.add(btnDefensaDerechaCentro);
 		    
 		    btnDefensaDerecha = new JButton("");
 		    btnDefensaDerecha.setBounds(403, 482, 85, 91);
+		    btnDefensaDerecha.setContentAreaFilled(false); 
+		    btnDefensaDerecha.setBorderPainted(false); 
+		    btnDefensaDerecha.setFocusPainted(false);
 		    PanelPlantilla.add(btnDefensaDerecha);
 		    
 		    btnCentroCampistaIzquierdo = new JButton("");
 		    btnCentroCampistaIzquierdo.setBounds(870, 343, 85, 91);
+		    btnCentroCampistaIzquierdo.setContentAreaFilled(false); 
+		    btnCentroCampistaIzquierdo.setBorderPainted(false); 
+		    btnCentroCampistaIzquierdo.setFocusPainted(false);
 		    PanelPlantilla.add(btnCentroCampistaIzquierdo);
 		    
 		    btnCentroCampistaDerechoCentro = new JButton("");
 		    btnCentroCampistaDerechoCentro.setBounds(525, 343, 85, 91);
+		    btnCentroCampistaDerechoCentro.setContentAreaFilled(false); 
+		    btnCentroCampistaDerechoCentro.setBorderPainted(false); 
+		    btnCentroCampistaDerechoCentro.setFocusPainted(false);
 		    PanelPlantilla.add(btnCentroCampistaDerechoCentro);
 		    
 		    btnCentroCampistaDerecho = new JButton("");
 		    btnCentroCampistaDerecho.setBounds(403, 343, 85, 91);
+		    btnCentroCampistaDerecho.setContentAreaFilled(false); 
+		    btnCentroCampistaDerecho.setBorderPainted(false); 
+		    btnCentroCampistaDerecho.setFocusPainted(false);
 		    PanelPlantilla.add(btnCentroCampistaDerecho);
 		    
 		    btnDelanteroDerecho = new JButton("");
 		    btnDelanteroDerecho.setBounds(735, 148, 85, 91);
+		    btnDelanteroDerecho.setContentAreaFilled(false); 
+		    btnDelanteroDerecho.setBorderPainted(false); 
+		    btnDelanteroDerecho.setFocusPainted(false);
 		    PanelPlantilla.add(btnDelanteroDerecho);
 		    
 		     btnDelanteroIzquierda = new JButton("");
 		    btnDelanteroIzquierda.setBounds(525, 148, 85, 91);
+		    btnDelanteroIzquierda.setContentAreaFilled(false); 
+		    btnDelanteroIzquierda.setBorderPainted(false); 
+		    btnDelanteroIzquierda.setFocusPainted(false);
 		    PanelPlantilla.add(btnDelanteroIzquierda);
 		    
 		     lblFondoPlantilla = new JLabel("");
