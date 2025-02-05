61e036f01652469981c69391098c6b79a70e0f1d
Fermin <fermin4002@gmail.com>
2025-02-05 14:06:29 +0100

    Nuevo Sql

--- ficheros/FutbolFantasy.sql
+++ ficheros/FutbolFantasy.sql
@@ -31,11 +31,111 @@ create table jugador (
     fuerza_defensa int(2) not null,
     fuerza_portero int(2) not null,
    
-    seleccionado boolean not null default false,
 	id_equipo int  null,
     
 	constraint PK_jugadores PRIMARY KEY(id_jugador),
     constraint FK_equipo foreign key(id_equipo)references equipo(id_equipo)
     
 );
+
+
+DELIMITER //
+create trigger asignacion_jugador
+after update on jugador 
+for each row
+begin 
+	if old.id_equipo is null and new.id_equipo is not null then
+		if upper(old.posicion) ="DEf" then 
+			 update equipo 
+			 set defensa=defensa-1 
+			 where id_equipo = new.id_equipo ;
+		elseif upper(old.posicion) ="POR" then 
+			 update equipo 
+			 set portero=portero-1 
+			 where id_equipo = new.id_equipo ;
+		elseif upper(old.posicion) ="MED" then 
+			 update equipo 
+			 set centrocampista=centrocampista-1 
+			 where id_equipo = new.id_equipo ;
+		elseif upper(old.posicion) ="DEL" then  
+			 update equipo 
+			 set delantero=delantero-1 
+			 where id_equipo = new.id_equipo ;
+		end if;
+	end if;    
+end;
+//
+
+
+
+create trigger limpieza_asignacion
+after update on jugador 
+for each row
+begin 
+	if old.id_equipo is not null and new.id_equipo is  null then
+		if upper(old.posicion) ="DEf" then 
+			 update equipo 
+			 set defensa=defensa+1 
+			 where id_equipo = old.id_equipo ;
+		elseif upper(old.posicion) ="POR" then 
+			 update equipo 
+			 set portero=portero+1 
+			 where id_equipo = old.id_equipo ;
+		elseif upper(old.posicion) ="MED" then 
+			 update equipo 
+			 set centrocampista=centrocampista+1 
+			 where id_equipo = old.id_equipo ;
+		elseif upper(old.posicion) ="DEL" then  
+			 update equipo 
+			 set delantero=delantero+1 
+			 where id_equipo = old.id_equipo ;
+		end if;
+	end if;    
+end;
+//
+
+
+
+create trigger reasignacion
+after update on jugador 
+for each row
+begin 
+	if old.id_equipo is not null and new.id_equipo is  not null then
+		if upper(old.posicion) ="DEf" then 
+			 update equipo 
+			 set defensa=defensa-1 
+			 where id_equipo = new.id_equipo ;
+             
+             update equipo 
+			 set defensa=defensa+1 
+			 where id_equipo = old.id_equipo ;
+		elseif upper(old.posicion) ="POR" then 
+			 update equipo 
+			 set portero=portero-1 
+			 where id_equipo = new.id_equipo ;
+             
+			 update equipo 
+			 set portero=portero+1 
+			 where id_equipo = old.id_equipo ;
+		elseif upper(old.posicion) ="MED" then 
+			 update equipo 
+			 set centrocampista=centrocampista-1 
+			 where id_equipo = new.id_equipo;
+             
+			 update equipo 
+			 set centrocampista=centrocampista+1 
+			 where id_equipo = old.id_equipo ;
+		elseif upper(old.posicion) ="DEL" then
+			 update equipo 
+			 set delantero=delantero-1 
+			 where id_equipo = new.id_equipo ;
+             
+			 update equipo 
+			 set delantero=delantero+1 
+			 where id_equipo = old.id_equipo ;
+		end if;
+	end if;    
+end;
+//
+DELIMITER ;
 
